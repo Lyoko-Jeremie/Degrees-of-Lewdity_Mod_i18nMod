37c13721aa6d1a1cf312e930f72d863a916df940
LyokoJeremie <lucheng989898@gmail.com>
2023-09-16 13:17:10 +0800

    ModI18NTypeB_JsCssMatcher

--- src/TypeB.ts
+++ src/TypeB.ts
@@ -3,8 +3,12 @@ interface TypeBOutputText {
     from: string;
     to: string;
 
+    // pos in passage
+    pos: number;
 
-    searchPatternRegex?: RegExp;
+    fileName?: string;
+    js?: boolean;
+    css?: boolean;
 }
 
 // come from GPT-4
@@ -89,7 +93,9 @@ interface TypeBInputStoryScript {
     // pos in passage
     pos: number;
 
-    searchPatternRegex?: RegExp;
+    fileName?: string;
+    js?: boolean;
+    css?: boolean;
 }
 
 class ModI18NTypeB_OutputTextMatcher {
@@ -201,70 +207,151 @@ class ModI18NTypeB_PassageMatcher {
             // console.log('ModI18NTypeB_PassageMatcher replacePassageContent passageName:', passageName);
             // console.log('ModI18NTypeB_PassageMatcher replacePassageContent before:', [s]);
             for (const v of pp) {
-                // console.log(v.passageName, v.pos, v.from.length);
 
-                // TODO NOTE this is a temp fix
-                // s = s.substring(0, Math.max(0, v.pos - 2)) + v.to + s.substring(Math.max(0, v.pos - 1) + v.from.length);
+                s = ModI18NTypeB_PassageMatcher.tryReplaceStringFuzzyWithHint(s, v, passageName);
 
-                // s = s.substring(0, v.pos) + v.to + s.substring(v.pos + v.from.length);
-
-                // first , we try to match and replace with const string in +-2 , this is the fastest way
-                if (s.substring(v.pos, v.pos + v.from.length) === v.from) {
-                    s = s.substring(0, v.pos) + v.to + s.substring(v.pos + v.from.length);
-                } else if (s.substring(v.pos - 1, v.pos + v.from.length - 1) === v.from) {
-                    s = s.substring(0, v.pos - 1) + v.to + s.substring(v.pos - 1 + v.from.length);
-                } else if (s.substring(v.pos - 2, v.pos + v.from.length - 2) === v.from) {
-                    s = s.substring(0, v.pos - 2) + v.to + s.substring(v.pos - 2 + v.from.length);
-                } else if (s.substring(v.pos + 1, v.pos + v.from.length + 1) === v.from) {
-                    s = s.substring(0, v.pos + 1) + v.to + s.substring(v.pos + 1 + v.from.length);
-                } else if (s.substring(v.pos + 2, v.pos + v.from.length + 2) === v.from) {
-                    s = s.substring(0, v.pos + 2) + v.to + s.substring(v.pos + 2 + v.from.length);
-                } else {
-                    // otherwise , we try to match and replace with fuzzy match in [-10~+30]
-                    try {
-                        let re: RegExp | undefined = new RegExp(ModI18NTypeB_escapedPatternString(v.from), '');
-                        // re.lastIndex = v.pos;
-                        const startPos = Math.max(0, v.pos - 10);
-                        const endPos = Math.min(s.length, v.pos + v.from.length + 30);
-                        const mm = re.exec(s.substring(startPos, endPos));
-                        if (mm) {
-                            const pStart = startPos + mm.index;
-                            const pEnd = pStart + v.from.length;
-                            s = s.substring(0, pStart) + v.to + s.substring(pEnd);
-                        } else {
-                            console.error('ModI18NTypeB_PassageMatcher replacePassageContent cannot find: ',
-                                [v.from], ' in ', [passageName], ' at ', [v.pos], ' in ', [s.substring(v.pos - 10, v.pos + v.from.length + 10)]);
-                        }
-                        re = undefined;
-                    } catch (e) {
-                        console.error(e);
-                        console.error('ModI18NTypeB_PassageMatcher replacePassageContent cannot find with error: ',
-                            [v.from], ' in ', [passageName], ' at ', [v.pos], ' in ', [s.substring(v.pos - 10, v.pos + v.from.length + 10)]);
-                    }
-                }
-
-                // let re: RegExp | undefined = new RegExp(ModI18NTypeB_ignoreSpaceString(v.from), '');
-                // re.lastIndex = v.pos - 2;
-                // const mm = re.exec(s);
-                // if (mm) {
-                //     if (mm.index > v.pos + 5) {
-                //         // too far
-                //     } else {
-                //         re.lastIndex = v.pos - 2;
-                //         s = s.replace(re, v.to);
-                //     }
-                // }
-                // re = undefined;
-
-                // const lastI = fuzzyMatchManual(s, v.from, Math.max(0, v.pos - 2));
-                // if (lastI !== -1) {
-                //     s = s.substring(0, Math.max(0, v.pos - 2)) + v.to + s.substring(lastI + v.from.length);
-                // }
             }
             // console.log('ModI18NTypeB_PassageMatcher replacePassageContent after:', [s]);
             return s;
         }
         return passageContent;
+    }
+
+    static tryReplaceStringFuzzyWithHint(s: string, v: { from: string, to: string, pos: number }, passageNameOrFileName: string) {
+        // first , we try to match and replace with const string in +-2 , this is the fastest way
+        if (s.substring(v.pos, v.pos + v.from.length) === v.from) {
+            s = s.substring(0, v.pos) + v.to + s.substring(v.pos + v.from.length);
+        } else if (s.substring(v.pos - 1, v.pos + v.from.length - 1) === v.from) {
+            s = s.substring(0, v.pos - 1) + v.to + s.substring(v.pos - 1 + v.from.length);
+        } else if (s.substring(v.pos - 2, v.pos + v.from.length - 2) === v.from) {
+            s = s.substring(0, v.pos - 2) + v.to + s.substring(v.pos - 2 + v.from.length);
+        } else if (s.substring(v.pos + 1, v.pos + v.from.length + 1) === v.from) {
+            s = s.substring(0, v.pos + 1) + v.to + s.substring(v.pos + 1 + v.from.length);
+        } else if (s.substring(v.pos + 2, v.pos + v.from.length + 2) === v.from) {
+            s = s.substring(0, v.pos + 2) + v.to + s.substring(v.pos + 2 + v.from.length);
+        } else {
+            // otherwise , we try to match and replace with fuzzy match in [-10~+30]
+            try {
+                let re: RegExp | undefined = new RegExp(ModI18NTypeB_escapedPatternString(v.from), '');
+                // re.lastIndex = v.pos;
+                const startPos = Math.max(0, v.pos - 10);
+                const endPos = Math.min(s.length, v.pos + v.from.length + 30);
+                const mm = re.exec(s.substring(startPos, endPos));
+                if (mm) {
+                    const pStart = startPos + mm.index;
+                    const pEnd = pStart + v.from.length;
+                    s = s.substring(0, pStart) + v.to + s.substring(pEnd);
+                } else {
+                    console.error('tryReplaceStringFuzzyWithHint cannot find: ',
+                        [v.from], ' in ', [passageNameOrFileName], ' at ', [v.pos], ' in ', [s.substring(v.pos - 10, v.pos + v.from.length + 10)]);
+                }
+                re = undefined;
+            } catch (e) {
+                console.error(e);
+                console.error('tryReplaceStringFuzzyWithHint cannot find with error: ',
+                    [v.from], ' in ', [passageNameOrFileName], ' at ', [v.pos], ' in ', [s.substring(v.pos - 10, v.pos + v.from.length + 10)]);
+            }
+        }
+        return s;
+    }
+
+}
+
+interface TypeBJsCssMatchInfo {
+    from: string;
+    to: string;
+
+    pos: number;
+
+    fileName?: string;
+    js?: boolean;
+    css?: boolean;
+}
+
+class ModI18NTypeB_JsCssMatcher {
+    constructor(
+        public OutputText: TypeBOutputText[],
+        public InputStoryScript: TypeBInputStoryScript[],
+    ) {
+        this.cssBuffer = new Map<string, TypeBJsCssMatchInfo[]>();
+        this.jsBuffer = new Map<string, TypeBJsCssMatchInfo[]>();
+        for (const v of OutputText) {
+            if (v.fileName) {
+                if (v.css) {
+                    if (this.cssBuffer.has(v.fileName)) {
+                        this.cssBuffer.get(v.fileName)!.push(v);
+                    } else {
+                        this.cssBuffer.set(v.fileName, [v]);
+                    }
+                }
+                if (v.js) {
+                    if (this.jsBuffer.has(v.fileName)) {
+                        this.jsBuffer.get(v.fileName)!.push(v);
+                    } else {
+                        this.jsBuffer.set(v.fileName, [v]);
+                    }
+                }
+            }
+        }
+        for (const v of InputStoryScript) {
+            if (v.fileName) {
+                if (v.css) {
+                    if (this.cssBuffer.has(v.fileName)) {
+                        this.cssBuffer.get(v.fileName)!.push(v);
+                    } else {
+                        this.cssBuffer.set(v.fileName, [v]);
+                    }
+                }
+                if (v.js) {
+                    if (this.jsBuffer.has(v.fileName)) {
+                        this.jsBuffer.get(v.fileName)!.push(v);
+                    } else {
+                        this.jsBuffer.set(v.fileName, [v]);
+                    }
+                }
+            }
+        }
+        for (const [k, v] of this.cssBuffer) {
+            this.cssBuffer.set(k, v.sort((a, b) => {
+                    // big to small
+                    return b.pos - a.pos;
+                }),
+            );
+        }
+        for (const [k, v] of this.jsBuffer) {
+            this.jsBuffer.set(k, v.sort((a, b) => {
+                    // big to small
+                    return b.pos - a.pos;
+                }),
+            );
+        }
+    }
+
+    cssBuffer: Map<string, TypeBJsCssMatchInfo[]>;
+    jsBuffer: Map<string, TypeBJsCssMatchInfo[]>;
+
+    replaceJs(content: string, fileName: string) {
+        const rep = this.jsBuffer.get(fileName);
+        if (rep) {
+            let s = content;
+            for (const v of rep) {
+                s = ModI18NTypeB_PassageMatcher.tryReplaceStringFuzzyWithHint(s, v, fileName);
+            }
+            return s;
+        }
+        return content;
+    }
+
+    replaceCss(content: string, fileName: string) {
+        const rep = this.cssBuffer.get(fileName);
+        if (rep) {
+            let s = content;
+            for (const v of rep) {
+                s = ModI18NTypeB_PassageMatcher.tryReplaceStringFuzzyWithHint(s, v, fileName);
+            }
+            return s;
+        }
+        return content;
     }
 
 }
@@ -277,9 +364,10 @@ class ModI18NTypeB {
     ) {
         this.outputTextMatchBuffer = new ModI18NTypeB_OutputTextMatcher(OutputText);
         this.inputStoryMatchBuffer = new ModI18NTypeB_PassageMatcher(InputStoryScript);
+        this.jsCssMatcher = new ModI18NTypeB_JsCssMatcher(OutputText, InputStoryScript);
 
         // monky patch
-        console.log('TypeB constructor monky patch document.createTextNode');
+        // console.log('TypeB constructor monky patch document.createTextNode');
         // this.oCreateTextNode = document.createTextNode;
         // document.createTextNode = (text: string) => {
         //     return this.oCreateTextNode.call(document, this.replaceOutputText(text));
@@ -290,6 +378,7 @@ class ModI18NTypeB {
 
     public outputTextMatchBuffer: ModI18NTypeB_OutputTextMatcher;
     public inputStoryMatchBuffer: ModI18NTypeB_PassageMatcher;
+    public jsCssMatcher: ModI18NTypeB_JsCssMatcher;
 
     replaceOutputText(text: string): string {
         if (!text.trim()) {
@@ -318,6 +407,14 @@ class ModI18NTypeB {
             }
         }
         return text;
+    }
+
+    replaceJs(text: string, fileName: string): string {
+        return this.jsCssMatcher.replaceJs(text, fileName);
+    }
+
+    replaceCss(text: string, fileName: string): string {
+        return this.jsCssMatcher.replaceCss(text, fileName);
     }
 
 }
