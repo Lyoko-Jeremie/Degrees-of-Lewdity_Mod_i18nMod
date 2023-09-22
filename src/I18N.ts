31f00d8fa7c6d89da5b6c21dac088d766b9c8c9a
LyokoJeremie <lucheng989898@gmail.com>
2023-09-22 08:18:01 +0800

    additionFile

--- src/I18N.ts
+++ src/I18N.ts
@@ -42,7 +42,7 @@ class ModI18N {
                 const zip = zips[i];
                 const info = zip.getModInfo();
                 console.log(' bootJson :', [info?.bootJson]);
-                if (info?.bootJson?.addstionFile.find(T => T === 'i18n.json')) {
+                if (info?.bootJson?.additionFile.find(T => T === 'i18n.json')) {
                     selfZip = zip;
                     break;
                 }
@@ -66,7 +66,7 @@ class ModI18N {
                 const zip = zips[i];
                 const info = zip.getModInfo();
                 console.log(' bootJson :', [info?.bootJson]);
-                if (info?.bootJson?.addstionFile.find(T => T === 'i18n.json')) {
+                if (info?.bootJson?.additionFile.find(T => T === 'i18n.json')) {
                     selfZip = zip;
                     break;
                 }
