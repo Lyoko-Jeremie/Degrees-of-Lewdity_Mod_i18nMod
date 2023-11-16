d9a525c99de1d82d13130ebf318e2d193d586cbf
LyokoJeremie <lucheng989898@gmail.com>
2023-11-16 10:17:15 +0800

    use ModuleCssReplacerAddon

--- src_boot/preload/preload.ts
+++ src_boot/preload/preload.ts
@@ -7,15 +7,15 @@
         window.modI18N.patchVersionString();
     });
 
-    const styleNode = document.querySelector<HTMLStyleElement>('#style-module-base');
-    if (!styleNode) {
-        console.error('cannot find #style-module-base');
-        return;
-    }
-    let style = styleNode.innerHTML;
-    style = style.replace('max-height: 2.4em;', 'max-height: 7em;');
-    style = style.replace('content: " months";', 'content: " 月数";');
-    style = style.replace('content: " weeks";', 'content: " 周数";');
-    styleNode.innerHTML = style;
+    // const styleNode = document.querySelector<HTMLStyleElement>('#style-module-base');
+    // if (!styleNode) {
+    //     console.error('cannot find #style-module-base');
+    //     return;
+    // }
+    // let style = styleNode.innerHTML;
+    // style = style.replace('max-height: 2.4em;', 'max-height: 7em;');
+    // style = style.replace('content: " months";', 'content: " 月数";');
+    // style = style.replace('content: " weeks";', 'content: " 周数";');
+    // styleNode.innerHTML = style;
 
 })();
