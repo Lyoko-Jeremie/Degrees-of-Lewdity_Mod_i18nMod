bd6c7ffe63db4d8c581dab3723079ac49649a3e5
LyokoJeremie <lucheng989898@gmail.com>
2023-12-09 15:50:06 +0800

    gczip

--- src_boot/earlyload/earlyload.ts
+++ src_boot/earlyload/earlyload.ts
@@ -1,5 +1,16 @@
 (async () => {
     console.log('ModI18N earlyload start');
 
+    window.modSC2DataManager.getSc2EventTracer().addCallback({
+        // @ts-ignore
+        modName: "ModI18N",
+        whenSC2PassageEnd: (passage: any, content: HTMLDivElement) => {
+            // console.log('ModI18N earlyload whenSC2PassageEnd', passage, content);
+            if (!window.modSC2DataManager.getModLoader().getModZip('ModI18N')?.gcIsReleased()) {
+                window.modSC2DataManager.getModLoader().getModZip('ModI18N')?.gcReleaseZip();
+            }
+        },
+    });
+
     await window.modI18N.readZipStream();
 })();
